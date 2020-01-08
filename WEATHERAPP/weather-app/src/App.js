cf9b8b39ed69becb6246292db91ba7f8b322fa56
caisser <Luis.Cardona@unisys.com>
2020-01-08 10:36:21 -0500

    qUITAR BOTON SINCO

--- WEATHERAPP/weather-app/src/App.js
+++ WEATHERAPP/weather-app/src/App.js
@@ -2,7 +2,6 @@ import React , { Component } from 'react';
 import Paper from  '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import {Grid, Col, Row} from 'react-flexbox-grid';
 import LocationList from './components/LocationList';
@@ -30,17 +29,6 @@ class App extends Component {
     this.setState({ city })
   }
 
-  llamarSINCO = () => {
-    const url = "http://10.2.193.77/SincoWSTransversal/ep/ePortalWSTrans/bdpsincows_CCPLU";
-    const json = {"COD_USUARI": "32526966", "PASSWORD1": "SEPTI2019"};
-    const miInit = {method : 'POST', headers: {'accept':'application/json', 'content-type':'application/json; charset=UTF-8'}, 
-    body: json };
-
-    fetch(url, miInit)
-    .then(response => console.log(response))
-    .catch(err => console.log(err +" "+ url));
-  }
-
   render() {
     const {city} = this.state;
 
@@ -53,7 +41,6 @@ class App extends Component {
                 <Typography variant='h6'>
                   Weather App
                 </Typography>
-                <Button className = "prueba" onClick ={this.llamarSINCO} variant="outlined" color="inherit">SINCO</Button>
               </Toolbar>
             </AppBar>
             </div>
